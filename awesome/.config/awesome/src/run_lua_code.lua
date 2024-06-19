09a9f97a0ad6b34d156cb10d3bcedb190bbc1f94
João Freitas <joaj.freitas@gmail.com>
2024-06-19 22:56:27 +0100

    Remove awesomewm config

--- awesome/.config/awesome/src/run_lua_code.lua
+++
@@ -1,8 +0,0 @@
-local naughty = require('naughty')
-
--- {{{
--- https://www.reddit.com/r/awesomewm/comments/6cwevs/the_purpose_of_run_lua_code/dhysrk9/
--- turn run Lua command into a calculator
-prt = function(...) naughty.notify{text=table.concat({...}, '\t')} end
-col = function(str) naughty.notify{text="   ", bg=str} end
--- }}}
