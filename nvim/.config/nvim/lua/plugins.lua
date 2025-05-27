2cb4567436d60a35a81b358a630f4caee9938aa8
João Freitas <joaj.freitas@gmail.com>
2025-05-27 22:21:57 +0100

    Remove commented code

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -185,19 +185,6 @@ M.setup = function()
 				},
 			},
 		},
-		--{
-		--  "NeogitOrg/neogit",
-		--  dependencies = {
-		--    "nvim-lua/plenary.nvim",         -- required
-		--    "sindrets/diffview.nvim",        -- optional - Diff integration
-		--    -- Only one of these is needed.
-		--    "nvim-telescope/telescope.nvim", -- optional
-		--  },
-		--  config = true
-		--},
-		--{
-		--    "ActivityWatch/aw-watcher-vim"
-		--},
 	})
 end
 
